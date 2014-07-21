bc2a8747e3fd7f3ef7ee24b835c0a2ef283adcf0
Thayne McCombs <astrothayne@gmail.com>
2014-07-20 23:15:48 -0600

    Fix %use-syntax to work for values other than package designators.
    Fixes #10

--- src/operators.lisp
+++ src/operators.lisp
@@ -4,6 +4,15 @@
   (defun package-syntax-name (package)
     (intern *standard-package-syntax-name* package)))
 
+(defun get-options (name)
+  "Get the options list for name, if it is available."
+  (get (cond ((typep name 'syntax) nil)
+	((and (typep name 'package-designator)
+	      (find-package name))
+	 (package-syntax-name name))
+	(t name))
+       :options))
+
 (defmacro defsyntax (name &body options)
   `(progn
      (setf (get ',name :options) ',options)
@@ -33,7 +42,7 @@
   (setq *readtable* (copy-readtable))
   (loop for name in names
         for syntax = (find-syntax name)
-        for options = (get (package-syntax-name name) :options)
+        for options = (get-options name)
         if (assoc :fuze (if (consp (car options)) options (cdr options))) do
           (handler-bind ((named-readtables:reader-macro-conflict
                             (lambda (_) (declare (ignore _))
